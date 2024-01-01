f17c72ea62b7c15cf2ac0b62514cbdf97ecbb27f
rumert <154933062+rumert@users.noreply.github.com>
2024-01-01 14:44:11 +0300

    fixed the searchbar's border

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ function SearchBar() {
   return (
     <div className='w-[90%] max-w-[900px] m-auto mb-4'>
       <div className='h-12 mx-4 border rounded-3xl overflow-hidden flex'>
-        <input type="text" placeholder='Search' className='w-full h-full px-2'/>
+        <input type="text" placeholder='Search' className='w-full h-full px-2 focus:border-2 focus:border-gray-500 focus:rounded-3xl outline-none'/>
         <button className='flex items-center justify-center h-12 w-[60px]'><PiMagnifyingGlass className='text-4xl' /></button>
       </div>      
     </div>
